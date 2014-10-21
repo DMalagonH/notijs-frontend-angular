c6edee9f266b3b3ae8c0833c60d833fe7b1fef6f
Diego Malagón <diegomalagonh@gmail.com>
2014-10-21 06:59:33 -0500

    Uso de scope en controlador de notificaciones

--- public/js/notijs/controllers.js
+++ public/js/notijs/controllers.js
@@ -1,68 +1,6 @@
 (function () {
 	var ctrl = angular.module('notijs.controllers', []);
 
-	ctrl.controller('NoticeController', function () {
-		this.notices = [
-			{
-				"id":		"1654sdfssk6",
-				"title":	"Título",
-				"body":		"Contenido de la notificación",
-				"datetime":	"2014-10-16T12:55:37.844Z",
-				"img":		"/img/info-icon.png",
-				"url":		"http://www.google.com",
-				"user_id":	"1467",
-				"read":		false
-			},
-			{
-				"id":		"sd545433dw45",
-				"title":	"Título segunda notificación",
-				"body":		"Contenido de la notificación",
-				"datetime":	"2014-10-16T08:00:00.000Z",
-				"img":		null,
-				"url":		"/url/a/aplicacion",
-				"user_id":	"1467",
-				"read":		true
-			},
-			{
-				"id":		"5d5a6fa657e4",
-				"title":	"Título tercera notificación",
-				"body":		"Contenido de la notificación",
-				"datetime":	"2014-10-16T08:00:00.000Z",
-				"img":		"/img/info-icon.png",
-				"url":		null,
-				"user_id":	"1467",
-				"read":		true
-			},
-		];
-
-		this.unread = this.notices.length;
-
-		this.go = function(url){
-			console.log("Ir a", url);
-		}
-
-		this.seeAllNotices = function(){
-			console.log("Ver todas las notificaciones");
-		};
-
-		this.markAllAsRead = function(){
-			console.log("Marcar todo como leído");
-		};
-
-		this.deleteAll = function(){
-			console.log("ELiminar todo");
-		};
-
-		this.markAsRead = function(id){
-			console.log("Marcar como leída", id);
-		};
-
-		this.delete = function(id){
-			console.log("Eliminar", id);
-		};
-	});
-
-	/*
 	ctrl.controller('NoticeController', ["$scope", function ($scope) {
 		$scope.notices = [
 			{
@@ -97,7 +35,7 @@
 			},
 		];
 
-		$scope.unread = this.notices.length;
+		$scope.unread = $scope.notices.length;
 
 		$scope.go = function(url){
 			console.log("Ir a", url);
@@ -123,5 +61,5 @@
 			console.log("Eliminar", id);
 		};
 	}]);
-*/
+
 })();
